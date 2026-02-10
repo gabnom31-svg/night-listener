44254613ebf28e68736d8fcd70abc01c00f8490d
JYYRT <gabnom31@gmail.com>
2026-02-10 11:50:26 +0800

    修改

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const btn = document.getElementById("startBtn");
 const hint = document.getElementById("hint");
 
 btn.onclick = () => {
-  hint.innerText = "我在听...";
+  hint.innerText = "我在听。";
+  btn.innerText = "正在听…";
   btn.disabled = true;
 };
